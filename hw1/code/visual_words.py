a3b2200adae9cef143d43a84ffe29f7f210837d5
Xuxin Cheng <chengxuxindavid@gmail.com>
2022-02-02 20:06:04 -0500

    init commit

---
+++ hw1/code/visual_words.py
@@ -0,0 +1,80 @@
+import os, multiprocessing
+from os.path import join, isfile
+
+import numpy as np
+from PIL import Image
+import scipy.ndimage
+import skimage.color
+import sklearn.cluster
+
+
+def extract_filter_responses(opts, img):
+    '''
+    Extracts the filter responses for the given image.
+
+    [input]
+    * opts    : options
+    * img    : numpy.ndarray of shape (H,W) or (H,W,3)
+    [output]
+    * filter_responses: numpy.ndarray of shape (H,W,3F)
+
+    [hint]
+    * To produce the expected collage, loop first over scales, then filter types, then color channel.
+    * Note the order argument when using scipy.ndimage.gaussian_filter. 
+    '''
+    
+    filter_scales = opts.filter_scales
+    # ----- TODO -----
+    pass
+
+def compute_dictionary_one_image(args):
+    '''
+    Extracts a random subset of filter responses of an image and save it to disk
+    This is a worker function called by compute_dictionary
+
+    Your are free to make your own interface based on how you implement compute_dictionary
+    '''
+
+    # ----- TODO -----
+    pass
+
+def compute_dictionary(opts, n_worker=1):
+    '''
+    Creates the dictionary of visual words by clustering using k-means.
+
+    [input]
+    * opts         : options
+    * n_worker     : number of workers to process in parallel
+    
+    [saved]
+    * dictionary : numpy.ndarray of shape (K,3F)
+    '''
+
+    data_dir = opts.data_dir
+    feat_dir = opts.feat_dir
+    out_dir = opts.out_dir
+    K = opts.K
+
+    #  For testing purpose, you can create a train_files_small.txt to only load a few images.
+    train_files = open(join(data_dir, 'train_files.txt')).read().splitlines()
+    # ----- TODO -----
+    pass
+
+    ## example code snippet to save the dictionary
+    # np.save(join(out_dir, 'dictionary.npy'), dictionary)
+
+def get_visual_words(opts, img, dictionary):
+    '''
+    Compute visual words mapping for the given img using the dictionary of visual words.
+
+    [input]
+    * opts    : options
+    * img    : numpy.ndarray of shape (H,W) or (H,W,3)
+    
+    [output]
+    * wordmap: numpy.ndarray of shape (H,W)
+    '''
+    
+    # ----- TODO -----
+    pass
+
